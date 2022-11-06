e05c2aff5ba1092999841dfd69d61b9aa223e038
Jose Pérez Cano <joseperez2000@hotmail.es>
2022-11-06 14:25:24 +0100

    Added docstrings

--- utils/postprocessing.py
+++ utils/postprocessing.py
@@ -5,8 +5,12 @@ Point = tuple[float,float]
 Contour = list[Point]
 Cell = tuple[int, int, Contour] # id, class
 
-"""NOT DOCUMENTED"""
 def get_N_closest_pairs_dists(a: Contour, b: Contour, N: int) -> list[float]:
+    """
+    Given two sets of points, 
+    returns the N closests pairs distances.
+    Complexity: O((n+m)log(m)), being m = max(|a|,|b|) and n = min(|a|,|b|)
+    """
     if len(a) > len(b):
         tree = KDTree(a)
         query_set = b
@@ -21,9 +25,17 @@ def get_N_closest_pairs_dists(a: Contour, b: Contour, N: int) -> list[float]:
         cpairs = cpairs[:N]
     return cpairs
 
-"""NOT DOCUMENTED"""
 def create_comparator(threshold: float, num_frontier: int) -> Callable[[Contour,Contour], bool]:
+    """
+    Returns comparator between two contours.
+    Two contours are equal if their num_frontier closest pairs 
+    are at a distance lower than threshold.
+    """
     def is_equal(a: Contour, b: Contour) -> bool:
+        """
+        Two contours are equal if their num_frontier closest pairs 
+        are at a distance lower than threshold.
+        """
         cpairs_dists = get_N_closest_pairs_dists(a, b, num_frontier)
         for dist in cpairs_dists:
             if dist > threshold:
@@ -31,8 +43,12 @@ def create_comparator(threshold: float, num_frontier: int) -> Callable[[Contour,
         return True
     return is_equal
 
-"""NOT DOCUMENTED"""
 def get_N_closest_pairs_idx(a: Contour, b: Contour, N: int) -> tuple[list[int],list[int]]:
+    """
+    Given two sets of points, 
+    returns the N closests pairs indices. 
+    Complexity: O((n+m)log(m)), being m = max(|a|,|b|) and n = min(|a|,|b|)
+    """
     is_a_tree = len(a) > len(b)
     if is_a_tree:
         tree = KDTree(a)
@@ -51,8 +67,13 @@ def get_N_closest_pairs_idx(a: Contour, b: Contour, N: int) -> tuple[list[int],l
         idxa, idxb = idxb, idxa
     return list(idxa), list(idxb)
 
-"""NOT DOCUMENTED"""
 def get_greatest_connected_component(idx: list[int], max_idx: int) -> tuple[int,int]:
+    """
+    Given a list of indices, returns the left and right value of the 
+    greatest connected component.
+    Two indices are considered connected if the differ in one unit.
+    0 and max_idx are considered connected.
+    """
     idx.sort()
     l_final, l_aux, r_final, r_aux = 0, 0, 0, 0
     found_comp = False
@@ -81,8 +102,11 @@ def get_greatest_connected_component(idx: list[int], max_idx: int) -> tuple[int,
             r_final, l_final = r_aux, l_aux
     return idx[l_final], idx[r_final]
 
-"""NOT DOCUMENTED"""
 def remove_idx(a: Cell, a_idx: list[int]) -> Cell:
+    """
+    Removes all the indices in a's contour such that they are included
+    in a_idx greatest connected component.
+    """
     left, right = get_greatest_connected_component(a_idx, len(a[2])-1)
     res = []
     if left > right: # Contour is in third position
@@ -92,21 +116,27 @@ def remove_idx(a: Cell, a_idx: list[int]) -> Cell:
         res.extend(a[2][0:left]) 
     return (a[0], a[1], res)
 
-"""NOT IMPLEMENTED"""
 def check_order(a: Cell, b: Cell) -> Cell:
+    """NOT IMPLEMENTED"""
     # Checks if joining a and b creates a cross in the middle.
     return True
 
-"""NOT DOCUMENTED"""
 def join(a: Cell, b: Cell) -> Cell:
+    """
+    Appends all the points in b's contour to a's contour.
+    Returns a new cell but it possibly modifies the input cells.
+    """
     good_order = check_order(a, b)
     if not good_order:
         b[2].reverse()
     a[2].extend(b[2])
     return a
 
-"""NOT DOCUMENTED"""
 def merge_cells(a: Cell, b: Cell) -> Cell:
+    """
+    Joins two cells by removing their 30 closest pairs
+    and connecting the endpoints.
+    """
     a_idx, b_idx = get_N_closest_pairs_idx(a[2], b[2], 30)
     a = remove_idx(a, a_idx)
     b = remove_idx(b, b_idx)
