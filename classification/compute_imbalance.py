b0a5d9e40565722c75e07b9109572ba250b6c1c4
Jose Pérez Cano <joseperez2000@hotmail.es>
2022-12-05 17:19:03 +0100

    Included license notice.

--- classification/compute_imbalance.py
+++ classification/compute_imbalance.py
@@ -3,6 +3,20 @@
 Script to train classification models.
 Right now only supports classification over nodes, without edges.
 
+Copyright (C) 2022  Jose Pérez Cano
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 import argparse
 from read_nodes import read_all_nodes
--- utils/postprocessing.py
+++ utils/postprocessing.py
@@ -1,3 +1,22 @@
+"""
+Module with utility functions for postprocessing.
+
+Copyright (C) 2022  Jose Pérez Cano
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
 from typing import Callable
 from scipy.spatial import KDTree
 import sys
