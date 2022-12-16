5245335898cfe2cc5c8ac921825ffb10ab265099
Jose Pérez Cano <joseperez2000@hotmail.es>
2022-12-16 18:42:32 +0100

    XGBoost CV

--- classification/train_xgboost.py
+++ classification/train_xgboost.py
@@ -20,13 +20,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 import argparse
 from read_nodes import create_node_splits
-import xgboost as xgb
+from xgboost import XGBClassifier
 import os
-import pickle
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split
 import numpy as np
-from typing import Tuple
+from typing import Dict, Any, Tuple
 from sklearn.model_selection import StratifiedKFold
+import pandas as pd
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -34,43 +35,51 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--graph-dir', type=str, required=True,
                      help='Folder containing .graph.csv files.')
 parser.add_argument('--val-size', type=float, default=0.2,
-                     help='Percentage of nodes given to validation set. Default: 0.2')
-parser.add_argument('--test-size', type=float, default=0.2,
-                     help='Percentage of nodes given to test set. Default: 0.2')
+                     help='Validation size used for early stopping. Default: 0.2')
 parser.add_argument('--seed', type=int, default=None,
                      help='Seed for random split. Default: None')
 parser.add_argument('--by-img', action='store_true',
                      help='Whether to separate images in the split. Default: False.')
-parser.add_argument('--num-workers', type=int, default=1)
-parser.add_argument('--cv-folds', type=int, default=10)
+parser.add_argument('--num-workers', type=int, default=1, 
+                     help='Number of processors to use. Default: 1.')
+parser.add_argument('--cv-folds', type=int, default=10, 
+                     help='Number of CV folds. Default: 10.')
 
-def logprob2prob(predt: np.ndarray) -> np.ndarray:
-    """
-    Converts log probabilities to probabilities.
-    """
-    exp_predt = np.exp(predt)
-    return exp_predt / (1 + exp_predt)
 
-def binarize(predt: np.ndarray, threshold: float) -> None:
-    """
-    Applies thresholding. [Inplace]
-    """
-    predt[predt >= threshold] = 1
-    predt[predt < threshold] = 0
+def train(
+    conf: Dict[str, Any], 
+    X_tr: np.ndarray, 
+    y_tr: np.ndarray, 
+    val_size: float,
+    seed: int
+    ) -> XGBClassifier:
+    X_tr, X_val, y_tr, y_val = train_test_split(
+        X_tr, y_tr, test_size=val_size, stratify=y_tr, random_state=seed
+    )
+    model = XGBClassifier(
+        n_estimators=conf['n_estimators'],
+        learning_rate=conf['learning_rate'], # 0.05
+        max_depth=conf['max_depth'],
+        colsample_bytree=conf['colsample_bytree'],
+        early_stopping_rounds=10
+    )
+    model.fit(X_tr, y_tr, eval_set=[(X_val, y_val)], verbose=False)
+    return model
 
-def my_f1_score(predt: np.ndarray, dtrain: xgb.DMatrix) -> Tuple[str, float]:
-    y = dtrain.get_label()
-    predt = logprob2prob(predt)
-    binarize(predt, 0.5)
-    f1 = f1_score(y, predt)
-    return 'F1_Score', f1 if f1 is not None else 0
+def evaluate(
+    model: XGBClassifier, 
+    X_val: np.ndarray,
+    y_val: np.ndarray
+    ) -> Tuple[float, float, float]:
+    preds = model.predict(X_val)
+    probs = model.predict_proba(X_val)[:,1]
+    f1 = f1_score(y_val, preds)
+    acc = accuracy_score(y_val, preds)
+    auc = roc_auc_score(y_val, probs)
+    return f1, acc, auc
 
-def my_accuracy(predt: np.ndarray, dtrain: xgb.DMatrix) -> Tuple[str, float]:
-    y = dtrain.get_label()
-    predt = logprob2prob(predt)
-    binarize(predt, 0.5)
-    acc = accuracy_score(y, predt)
-    return 'Accuracy', acc if acc is not None else 0
+def save(metrics: Dict[str,Tuple[float,float,float]], path: str) -> None:
+    metrics.to_csv(path, index=False)
 
 if __name__=='__main__':
     args = parser.parse_args()
@@ -78,41 +87,55 @@ if __name__=='__main__':
 
     X_train, X_val, X_test, y_train, y_val, y_test = \
         create_node_splits(
-            GRAPH_DIR, args.val_size, args.test_size, args.seed, 
+            GRAPH_DIR, 0.2, 0.2, 0, 
             'by_img' if args.by_img else 'total'
         )
 
-    X = np.vstack((X_train, X_val))
-    y = np.hstack((y_train, y_val))
+    X = np.vstack((X_train, X_val, X_test))
+    y = np.hstack((y_train, y_val, y_test))
     skf = StratifiedKFold(n_splits=args.cv_folds)
     skf.get_n_splits(X, y)
 
-    for train_index, test_index in skf.split(X, y):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        ## Train
-
-        ## Save metrics
-
-
-    dtrain = xgb.DMatrix(X_train, label=y_train)
-    dval = xgb.DMatrix(X_val, label=y_val)
-    evallist = [(dtrain, 'train'), (dval, 'eval')]
-
-    param = {'max_depth': 6, 'eta': 0.1, 'objective': 'binary:logistic'}
-    param['nthread'] = args.num_workers
-    param['eval_metric'] = 'auc'
-
-    num_round = 100
-    evals_result = {}
-    bst = xgb.train(
-        param, dtrain, num_round, evals=evallist, feval=my_f1_score,
-        early_stopping_rounds=10, evals_result=evals_result
+    metrics = pd.DataFrame(
+        {}, 
+        columns=[
+            'n_estimators', 'learning_rate', 'max_depth', 'colsample_bytree',
+            'f1', 'accuracy', 'auc'
+        ]
     )
-
-    with open(os.path.join(FILE_DIR, 'logs/0000.pickle'), 'wb') as f:
-        pickle.dump(evals_result, f)
-
-    bst.save_model(os.path.join(FILE_DIR,'weights/0000.model'))
-
+    confs = [
+        {'n_estimators': 100,'learning_rate': 0.05,'max_depth': 4,'colsample_bytree': 0},
+        {'n_estimators': 500,'learning_rate': 0.05,'max_depth': 4,'colsample_bytree': 0},
+        {'n_estimators': 100,'learning_rate': 0.05,'max_depth': 4,'colsample_bytree': 0},
+        {'n_estimators': 500,'learning_rate': 0.05,'max_depth': 4,'colsample_bytree': 0},
+        {'n_estimators': 100,'learning_rate': 0.05,'max_depth': 8,'colsample_bytree': 0},
+        {'n_estimators': 500,'learning_rate': 0.05,'max_depth': 8,'colsample_bytree': 0},
+        {'n_estimators': 100,'learning_rate': 0.05,'max_depth': 8,'colsample_bytree': 0.5},
+        {'n_estimators': 500,'learning_rate': 0.05,'max_depth': 8,'colsample_bytree': 0.5},
+        {'n_estimators': 100,'learning_rate': 0.05,'max_depth': 16,'colsample_bytree': 0.5},
+        {'n_estimators': 500,'learning_rate': 0.05,'max_depth': 16,'colsample_bytree': 0.5},
+        {'n_estimators': 100,'learning_rate': 0.05,'max_depth': 16,'colsample_bytree': 0.5},
+        {'n_estimators': 500,'learning_rate': 0.05,'max_depth': 16,'colsample_bytree': 0.5}
+    ]
+    for k, conf in enumerate(confs):
+        print('Configuration {:2}/{:2}'.format(k, len(confs)), end='\r')
+        f1_mean, acc_mean, auc_mean = 0, 0, 0
+        for train_index, val_index in skf.split(X, y):
+            X_train, X_cval = X[train_index], X[val_index]
+            y_train, y_cval = y[train_index], y[val_index]
+            ## Train
+            model = train(conf, X_train, y_train, args.val_size, args.seed)
+            ## Save metrics
+            f1, acc, auc = evaluate(model, X_cval, y_cval)
+            f1_mean += f1; acc_mean += acc; auc_mean += auc
+        f1_mean /= args.cv_folds; acc_mean /= args.cv_folds; auc_mean /= args.cv_folds
+        tmp = pd.DataFrame(
+            [(*conf.values(), f1_mean, acc_mean, auc_mean)], 
+            columns=[
+                'n_estimators', 'learning_rate', 'max_depth', 'colsample_bytree',
+                'f1', 'accuracy', 'auc'
+                ]
+        )
+        metrics = pd.concat((metrics, tmp))
+    save(metrics, 'tmp.csv')
     
