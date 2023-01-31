b8d44074c602d990331153def531aaed247dedf9
Jose Pérez Cano <joseperez2000@hotmail.es>
2023-01-31 17:34:54 +0100

    Added test [actions skip]

---
+++ tests/hov_prob_test.py
@@ -0,0 +1,41 @@
+"""
+Copyright (C) 2023  Jose Pérez Cano
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact information: joseperez2000@hotmail.es
+"""
+import pytest
+import sys
+import os
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PKG_DIR = os.path.dirname(TEST_DIR)
+sys.path.append(PKG_DIR)
+
+from utils.preprocessing import parse_path, get_names, read_json
+
+JSON_DIR = parse_path(TEST_DIR) + 'json/'
+
+@pytest.mark.parametrize('name', get_names(JSON_DIR, '.json'))
+def test_hovernet_patches_5dim(name):
+    nuc = read_json(os.path.join(JSON_DIR, name + '.json'))
+    for inst in nuc:
+        inst_info = nuc[inst]
+        inst_centroid = inst_info['centroid']
+        inst_type = inst_info['type']
+        if inst_type != 0:
+            prob1 = inst_info['prob1']
+            pred_type = (prob1 > 0.5) * 1 + 1
+            assert int(inst_type) == int(pred_type)
