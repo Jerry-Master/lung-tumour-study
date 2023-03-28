b0e45eb5746ddeba02317cae305a10602c6a0b3a
Jose Pérez Cano <joseperez2000@hotmail.es>
2023-03-28 17:56:00 +0200

    Added eval pipe [actions skip]

--- tumourkit/eval_pipe.py
+++ tumourkit/eval_pipe.py
@@ -41,7 +41,7 @@ def run_evaluation(args: Namespace, logger: Logger) -> None:
             names = os.path.join(args.root_dir, 'data', split, 'names.txt'),
             gt_path = os.path.join(args.root_dir, 'data', split, 'centroids'),
             pred_path = os.path.join(args.root_dir, 'data', split, 'centroids_hov'),
-            save_name = args.save_name,
+            save_name = args.save_name + '_' + split,
             debug_path = None,
         )
         eval_segment(newargs)
