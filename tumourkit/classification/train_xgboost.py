335888fdf3b969665736cf81397213b69de49532
Jose Pérez Cano <joseperez2000@hotmail.es>
2023-04-11 11:11:03 +0200

    Bug fix [actions skip]

--- tumourkit/classification/train_xgboost.py
+++ tumourkit/classification/train_xgboost.py
@@ -204,13 +204,13 @@ def main_with_args(args: Namespace, logger: Logger):
                 future = executor.submit(cross_validate, args, conf, skf, X, y)
                 futures.append(future)
             for k, future in enumerate(futures):
-                logger.info('Configuration {:2}/{:2}'.format(k, len(confs)))
+                logger.info('Configuration {:2}/{:2}'.format(k + 1, len(confs)))
                 tmp = future.result()
                 metrics = pd.concat((metrics, tmp))
                 save(metrics, args.save_name + '.csv')
     else:
         for k, conf in enumerate(confs):
-            logger.info('Configuration {:2}/{:2}'.format(k, len(confs)))
+            logger.info('Configuration {:2}/{:2}'.format(k + 1, len(confs)))
             tmp = cross_validate(args, conf, skf, X, y)
             metrics = pd.concat((metrics, tmp))
             save(metrics, args.save_name + '.csv')
