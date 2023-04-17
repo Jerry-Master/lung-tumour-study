2f6cd93387e14f2c522c326a1f71d7aa56434fd5
Jose Pérez Cano <joseperez2000@hotmail.es>
2023-04-17 09:49:02 +0200

    Improved metrics [actions skip]

--- tumourkit/eval_pipe.py
+++ tumourkit/eval_pipe.py
@@ -72,7 +72,7 @@ def compute_ece(args: Namespace, logger: Logger, split: str) -> None:
         else:
             probs = np.vstack((probs, _probs))
     preds = np.argmax(probs, axis=1).reshape((-1, 1))
-    metrics = metrics_from_predictions(trues, preds, probs)
+    metrics = metrics_from_predictions(trues, preds, probs, args.num_classes)
     if args.num_classes == 2:
         acc, f1, auc, perc_error, ece = metrics
         dic_metrics = {
