1e87041ea9ecf2e817e3f9d715a37dc387037245
Jose Pérez Cano <joseperez2000@hotmail.es>
2023-05-18 09:57:11 +0200

    Copyleft [actions skip]

--- tumourkit/preprocessing/remove_uncertain.py
+++ tumourkit/preprocessing/remove_uncertain.py
@@ -1,3 +1,23 @@
+"""
+Removes cells with label uncertain from the annotations.
+
+Copyright (C) 2023  Jose Pérez Cano
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Contact information: joseperez2000@hotmail.es
+"""
 import os
 from typing import List, Dict, Any
 from tqdm import tqdm
